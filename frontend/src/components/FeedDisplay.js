6e34a1816e6e93c79fd0046c8b6605122b0c4497
James Kim <73400219+shallowsmith@users.noreply.github.com>
2024-05-06 05:09:58 -0700

    Update FeedDisplay.js

--- frontend/src/components/FeedDisplay.js
+++ frontend/src/components/FeedDisplay.js
@@ -6,7 +6,7 @@ function FeedDisplay() {
     useEffect(() => {
         const fetchPosts = async () => {
             try {
-                const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+                const response = await fetch('http://127.0.0.1:5000/get_posts', {
                     method: 'GET',
                     headers: {
                         'Content-Type': 'application/json'
