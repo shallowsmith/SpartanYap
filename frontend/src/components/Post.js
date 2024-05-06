115e10919e43d41b7f1d2aee75843dc5debd6e06
James <hukk022@hotmail.com>
2024-05-06 04:16:11 -0700

    Implement post styling

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -56,18 +56,33 @@ export default function Post() {
   }
 
   return (
-    <form onSubmit={onSubmit}>
-      <div>
-        <label htmlFor="postContent">Post</label>
-        <input
-          type="text"
-          id="postContent"
-          value={postContent}
-          onChange={(e) => setPostContent(e.target.value)}
-          placeholder="What's on your mind?"
-        />
-      </div>
-      <button type="submit">Create</button>
-    </form>
+    <div className="flex justify-center items-center">
+      <form onSubmit={onSubmit} className="space-y-4 w-3/5 mt-7 mb-7">
+        <div className="w-full border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
+          <div className="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
+            <label htmlFor="postContent" className="sr-only">
+              Your Post
+            </label>
+            <textarea
+              id="postContent"
+              rows="4"
+              className="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
+              placeholder="What's on your mind yapper?"
+              required
+              value={postContent}
+              onChange={(e) => setPostContent(e.target.value)}
+            ></textarea>
+          </div>
+          <div className="flex items-center justify-between px-3 py-2 border-t dark:border-gray-600">
+            <button
+              type="submit"
+              className="inline-flex items-center py-2 px-4 text-xs font-medium text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800"
+            >
+              YAP ON!
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
   );
 }
